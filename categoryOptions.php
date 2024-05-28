a12f63f559496f6abb33376b53642ee0ab6e8a25
akulapallavi <akulapallu1234@gmail.com>
2024-05-28 20:17:19 +0530

    dailywagesweb

---
+++ categoryOptions.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<option>- All Job Postings -</option>
+<option>plumbing</option>
+<option>construction</option>
+<option>electrician</option>
+<option>fall ceiling</option>
+<option>Cooking</option>
+<option>house maid</option>
+<option>sewage cleaner</option>
+
---
+++ industryOptions.php
@@ -0,0 +1,10 @@
+<?php
+
+?>
+<option>plumbing,cleaning</option>
+<option>construction</option>
+<option>maids</option>
+<option>fall ceiling, construction</option>
+<option>housewarming</option>
+
+
